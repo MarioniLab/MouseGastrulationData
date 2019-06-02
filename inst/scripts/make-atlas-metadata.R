4e61ac1abcf1bb1eb7627dc76b9ea0e5a9aa8969
Jonathan Griffiths <jonathan.griffiths.94@gmail.com>
2019-06-02 15:44:04 +0100

    Added make-data scripts for atlas and WT chimera

---
+++ inst/scripts/make-atlas-metadata.R
@@ -0,0 +1,36 @@
+info <- data.frame(
+    Title = sprintf("Atlas %s", 
+        c("processed counts", "rowData", "colData", "size factors", 
+            sprintf("raw counts (sample %i)", seq_len(4)))
+    ),
+    Description = sprintf("%s for the mouse embryo atlas single-cell RNA-seq dataset", 
+        c("Processed count matrix", "Per-gene metadata", "Per-cell metadata", "Size factors",
+            sprintf("Raw counts for sample %i", seq_len(4)))
+    ),
+    RDataPath = file.path("MouseGastrulationData", "atlas", "1.0.0", 
+        c("counts-processed-all.rds", "rowdata.rds", "coldata.rds", "sizefac.rds",
+            sprintf("counts-raw-sample%i.rds", seq_len(4)))
+    ),
+    BiocVersion="3.10",
+    Genome="mm10",
+    SourceType="TXT",
+    SourceUrl=rep(
+        c("https://content.cruk.cam.ac.uk/jmlab/atlas_data"),
+        c(4)
+    ),
+    SourceVersion=paste(
+        c("raw_counts.mtx.gz", "genes.tsv.gz", "meta.tab.gz", "sizefactors.tab.gz", 
+            sprintf("sample_%i_unswapped.mtx.gz", seq_len(4))),
+        sep=";"
+    ),
+    Species="Mus musculus",
+    TaxonomyId="10090",
+    Coordinate_1_based=TRUE,
+    DataProvider="Jonathan Griffiths",
+    Maintainer="Jonathan Griffiths <jonathan.griffiths.94@gmail.com>",
+    RDataClass="character",
+    DispatchClass="Rds",
+    stringsAsFactors = FALSE
+)
+
+write.csv(file="../extdata/metadata-atlas.csv", info, row.names=FALSE)
