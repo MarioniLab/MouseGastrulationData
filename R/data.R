1767caee7e2e869dea6f05dd85e5a494839b8863
Jonathan Griffiths <jonathan.griffiths.94@gmail.com>
2019-06-19 15:46:19 +0100

    Moved celltype colours to rda

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Celltype colours from Pijuan-Sala et al.
+#'
+#' A vector containing the colour hexcodes that were used
+#' in Pijuan-Sala et al., for quick plotting of the data.
+#'
+#' @format A vector of hexcodes named according to the appropriate
+#' celltype; celltypes match those in the metadata.
+#' 
+#' @references
+#' Pijuan-Sala B, Griffiths JA, Guibentif C et al. (2019).
+#' A single-cell molecular map of mouse gastrulation and early organogenesis.
+#' \emph{Nature} 566, 7745:490-495.
+"embryo_celltype_colours"
