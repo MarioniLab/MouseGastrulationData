deb1d1e4e5155df22fd95fa703648541d312cf16
Jonathan Griffiths <jonathan.griffiths@genomicsplc.com>
2020-04-06 09:57:29 +0100

    Tidied code, fixed issue with reducedDim naming

--- R/getProcOrRaw.R
+++ R/getProcOrRaw.R
@@ -20,27 +20,31 @@
     rowdata <- hub[hub$rdatapath==file.path(host, "rowdata.rds")][[1]]
 
     if (type=="processed"){
-        count_list <- lapply(samples, function(i){
-            hub[hub$rdatapath==file.path(host, sprintf("counts-processed-sample%s.rds", i))][[1]]
+        # Temporary function for data extraction
+        EXTRACTOR <- function(target) {
+            lapply(samples, function(i){
+                hub[hub$rdatapath==file.path(host, sprintf("%s-sample%s.rds", target, i))][[1]]
+            })
+        }
+        count_list <- EXTRACTOR("counts-processed")
+        coldata_list <- EXTRACTOR("coldata")
+        sf_list <- EXTRACTOR("sizefac")
+        reducedDims_list <- EXTRACTOR("reduced-dims")
+
+        # Handle data with different reducedDim names
+        reducedDims_names = names(reducedDims_list[[1]])
+        reducedDims_combined = lapply(reducedDims_names, function(x){
+            do.call(rbind, lapply(reducedDims_list, function(y) y[[x]]))
         })
-        coldata_list <- lapply(samples, function(i){
-            hub[hub$rdatapath==file.path(host, sprintf("coldata-sample%s.rds", i))][[1]]
-        })
-        sf_list <- lapply(samples, function(i){
-            hub[hub$rdatapath==file.path(host, sprintf("sizefac-sample%s.rds", i))][[1]]
-        })
-        reducedDims_list <- lapply(samples, function(i){
-            hub[hub$rdatapath==file.path(host, sprintf("reduced-dims-sample%s.rds", i))][[1]]
-        })
-        combined_pca <- do.call(rbind, lapply(reducedDims_list, function(x) x$pca.corrected))
-        combined_umap <- do.call(rbind, lapply(reducedDims_list, function(x) x$umap))
+        names(reducedDims_combined) = reducedDims_names
+
         sce <- SingleCellExperiment(
                 assays=list(counts=do.call(cbind, count_list)),
                 colData=as(do.call(rbind, coldata_list), "DataFrame"),
-                reducedDims=list(pca.corrected = combined_pca, umap = combined_umap),
+                reducedDims=reducedDims_combined,
                 rowData=rowdata
             )
-        sizeFactors(sce) <- do.call(c, sf_list)
+        sizeFactors(sce) <- unlist(sf_list)
         rownames(sce) <- rowData(sce)$ENSEMBL
         colnames(sce) <- colData(sce)$cell
         return(sce)
